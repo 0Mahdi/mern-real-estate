fe3145f9cc2c6eff3b45306ff3eafd4c1bb024eb
Mahdi Hossen <114096228+0Mahdi@users.noreply.github.com>
2024-03-12 00:38:14 +0600

    complete show user listings functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -8,7 +8,6 @@ export const test = (req, res) => {
     message: 'Api route is working!',
   });
 };
-
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
     return next(errorHandler(401, 'You can only update your own account!'));
@@ -16,7 +15,6 @@ export const updateUser = async (req, res, next) => {
     if (req.body.password) {
       req.body.password = bcryptjs.hashSync(req.body.password, 10);
     }
-
     const updatedUser = await User.findByIdAndUpdate(
       req.params.id,
       {
@@ -29,36 +27,33 @@ export const updateUser = async (req, res, next) => {
       },
       { new: true }
     );
-
     const { password, ...rest } = updatedUser._doc;
-
     res.status(200).json(rest);
   } catch (error) {
     next(error);
   }
 };
-
-
 export const deleteUser = async (req, res, next) => {
-  if (req.user.id !== req.params.id) return next(errorHandler(401, 'You can only delete your own account!'))
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, 'You can only delete your own account!'));
   try {
-    await User.findByIdAndDelete(req.params.id)
+    await User.findByIdAndDelete(req.params.id);
     res.clearCookie('access_token');
-    res.status(200).json('User has been deleted');
+    res.status(200).json('User has been deleted!');
   } catch (error) {
-    next(error)
-  } 
+    next(error);
+  }
 };
 
-export const getUserListing = async(req, res, next) => {
+export const getUserListings = async (req, res, next) => {
   if (req.user.id === req.params.id) {
     try {
-      const listings = await Listing.find({ userRef: req.params.id});
+      const listings = await Listing.find({ userRef: req.params.id });
       res.status(200).json(listings);
     } catch (error) {
-      next(error)
+      next(error);
     }
-  }else{
-    return next(errorHandler(401, 'You can only view your own listing!'));
+  } else {
+    return next(errorHandler(401, 'You can only view your own listings!'));
   }
-}
+};
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,27 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import Profile from './pages/Profile'
-import About from './pages/About'
-import Header from './components/Header'
-import PrivateRoute from './components/PrivateRoute'
-import CreateListing from './pages/CreateListing'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Header from './components/Header';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import PrivateRoute from './components/PrivateRoute';
+import Profile from './pages/Profile';
+import CreateListing from './pages/CreateListing';
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
-    <Routes>
+      <Header />
+      <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
-        <Route element={<PrivateRoute/>}>
+        <Route path='/about' element={<About />} />
+        <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
           <Route path='/create-listing' element={<CreateListing />} />
         </Route>
-    </Routes>
-  </BrowserRouter>
-  )
+      </Routes>
+    </BrowserRouter>
+  );
 }
